87e0196d89499a5a9314ab9340e47f67caa2aa42
malit <malitzinjrm16@gmail.com>
2026-06-09 13:56:12 -0600

    K

--- src/main/java/com/upiiz/mascotas/Controllers/VistasController.java
+++ src/main/java/com/upiiz/mascotas/Controllers/VistasController.java
@@ -6,13 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VistasController {
 
-    @GetMapping("/mantenimiento-mascotas")
-    public String mostrarMascotas() {
-        return "mascotas";
+    // 1. Esta ruta responderá cuando escribas /mascotas al final de la URL
+    @GetMapping("/mascotas")
+    public String mascotas() {
+        return "mascotas"; // Busca el archivo templates/mascotas.html
     }
 
+    // 2. AGREGA ESTO: Redirige automáticamente la raíz (/) hacia /mascotas
     @GetMapping("/")
-    public String redirigir() {
+    public String raiz() {
         return "redirect:/mascotas";
     }
 }
